5250e482ebcb3b21860f7ed41b41eb35d922da3e
raviavaiya3 <21bmiit007@gmail.com>
2024-09-29 17:01:29 +0530

    Practice

---
+++ Flask/Flask_app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import numpy as np
+
+app=Flask(__name__)
+
+# @app.route('/<int:number>/')
+# def  increment(number):
+#     print("API called number",number)
+#     return "Increament number is "+str(number+1)
+
+
+# @app.route('/<string:name>/')
+# def  increment(name):
+#     print("API called name",name)
+#     return "my name is "+str(name)
+
+details=[
+    {'name':'Ravi','address':'surat'}
+]
+
+@app.route('/person', methods=['GET'])
+def get_person():
+    return jsonify(details)
+
+if __name__ == "__main__":
+    app.run(debug=True)
