ea6ba7ffde1f56f223e93e41469698773da5cada
Ravi Avaiya <110326463+raviavaiya@users.noreply.github.com>
2024-11-21 12:28:04 +0530

    DS Project

--- Data-Science-Project-1/churn_app.py
+++ Data-Science-Project-1/churn_app.py
@@ -1,9 +1,55 @@
 import streamlit as st
+import pandas as pd
 from joblib import load
+from sklearn.preprocessing import LabelEncoder
 
+# Load the trained Random Forest model
 model = load('random_forest_model.joblib')
 
+# Create a Streamlit app
 st.title("Customer Churn Prediction App")
 
-st.header("Enter Customer Details") 
+# Input fields for feature values on the main screen
+st.header("Enter Customer Information")
+tenure = st.number_input("Tenure (in months)", min_value=0, max_value=100, value=1)
+internet_service = st.selectbox("Internet Service", ('DSL', 'Fiber optic', 'No'))
+contract = st.selectbox("Contract", ('Month-to-month', 'One year', 'Two year'))
+monthly_charges = st.number_input("Monthly Charges", min_value=0, max_value=200, value=50)
+total_charges = st.number_input("Total Charges", min_value=0, max_value=10000, value=0)
 
+# Map input values to numeric using the label mapping
+label_mapping = {
+    'DSL': 0,
+    'Fiber optic': 1,
+    'No': 2,
+    'Month-to-month': 0,
+    'One year': 1,
+    'Two year': 2,
+}
+internet_service = label_mapping[internet_service]
+print("internet_service  label-----",internet_service)
+contract = label_mapping[contract]
+print("contract  label-----",contract)
+
+# Make a prediction using the model
+prediction = model.predict([[tenure, internet_service, contract, monthly_charges, total_charges]])
+
+# Display the prediction result on the main screen
+# st.header("Prediction Result")
+# if prediction[0] == 0:
+#     st.success("This customer is likely to stay.")
+# else:
+#     st.error("This customer is likely to churn.")
+
+
+# Create a submit button
+if st.button("Submit"):
+    # Make a prediction using the model
+    prediction = model.predict([[tenure, internet_service, contract, monthly_charges, total_charges]])
+
+    # Display the prediction result on the main screen
+    st.header("Prediction Result")
+    if prediction[0] == 0:
+        st.success("This customer is likely to stay.")
+    else:
+        st.error("This customer is likely to churn.")
